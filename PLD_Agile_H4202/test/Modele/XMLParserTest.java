b8db305dd31982005f2c139d7e5321039f9dba12
A-Kira <A-Kira@192.168.1.23>
2017-11-05 21:55:04 +0100

    TestUnitaire reesayer

--- PLD_Agile_H4202/test/Modele/XMLParserTest.java
+++ PLD_Agile_H4202/test/Modele/XMLParserTest.java
@@ -104,7 +104,7 @@ public class XMLParserTest {
         
         
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+       
         fail("The test case is a prototype.");
     }
 
