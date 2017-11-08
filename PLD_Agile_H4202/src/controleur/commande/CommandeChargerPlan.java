48ef20aef35a71b88e8a863bb5bdc71b3947d5d9
ASUS <ASUS@DESKTOP-47HV2QT>
2017-11-08 19:19:34 +0100

    REGLES DE NOMMAGES CORRIGEES

--- PLD_Agile_H4202/src/controleur/commande/CommandeChargerPlan.java
+++ PLD_Agile_H4202/src/controleur/commande/CommandeChargerPlan.java
@@ -20,12 +20,12 @@ public class CommandeChargerPlan extends CommandeNonAnnulable {
     /**
      * Le contrôleur de données
      */
-    private final ControleurDonnees controleurDonnees;
+    private final ControleurDonnees CONTROLEUR_DONNEES;
     
     /**
      * Le fichier de plan
      */
-    private final File planFichier;
+    private final File PLAN_FICHIER;
 
     /**
      * Le constructeur du chargement du plan
@@ -33,22 +33,22 @@ public class CommandeChargerPlan extends CommandeNonAnnulable {
      * @param plan Le fichier de plan
      */
     public CommandeChargerPlan(ControleurDonnees controleurDonnees, File plan) {
-        this.controleurDonnees = controleurDonnees;
-        planFichier = plan;
+        this.CONTROLEUR_DONNEES = controleurDonnees;
+        PLAN_FICHIER = plan;
     }
 
     @Override
     public void executer() throws CommandeException {
         try {
             // Remplacer plan qui est chargé d'un nouveau plan (si et seulement le chargement du xml a reussi)
-            controleurDonnees.setPlan(XMLParser.getInstance().getPlan(planFichier));
+            CONTROLEUR_DONNEES.setPlan(XMLParser.getInstance().getPlan(PLAN_FICHIER));
 
-            controleurDonnees.notifierObservateursActivation(false);
-            controleurDonnees.notifierObservateurOuvrirDemande(true);
-            controleurDonnees.notifierObservateursCalculTournee(false);
-            controleurDonnees.notifierPlanChargeObservateur();
-            controleurDonnees.notifierObservateursMessage(String.format("Plan de la ville (%s) chargé avec succès ! Veuillez charger la demande de livraison maintenant.", planFichier.getName()));
-            controleurDonnees.effacerHistorique();
+            CONTROLEUR_DONNEES.notifierObservateursActivation(false);
+            CONTROLEUR_DONNEES.notifierObservateurOuvrirDemande(true);
+            CONTROLEUR_DONNEES.notifierObservateursCalculTournee(false);
+            CONTROLEUR_DONNEES.notifierPlanChargeObservateur();
+            CONTROLEUR_DONNEES.notifierObservateursMessage(String.format("Plan de la ville (%s) chargé avec succès ! Veuillez charger la demande de livraison maintenant.", PLAN_FICHIER.getName()));
+            CONTROLEUR_DONNEES.effacerHistorique();
         } catch (SAXException | JDOMException|ExceptionXML |ParserConfigurationException| IOException | ParseException ex) {
             throw new CommandeException(ex.getMessage());
         }
